2fcb184da96a33227395ff261cfb57f974156244
Garv Saxena <garvsaxena185@gmail.com>
2024-01-30 22:33:51 +0530

    P1
    1. Write a Java Program to Print "Hello World! This is my First Java Program."

---
+++ P1/src/p1/P1.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package p1;
+
+
+public class P1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! This is my First Java Program");
+    }
+    
+}
