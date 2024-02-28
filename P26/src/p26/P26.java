b11e61eb0c538ae4ab08b59eb0fc5a97782811ae
Garv Saxena <garvsaxena185@gmail.com>
2024-02-28 16:12:10 +0530

    p26

---
+++ P26/src/p26/P26.java
@@ -0,0 +1,17 @@
+
+package p26;
+
+public class P26 {
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        try{
+        arr[6]=55;
+        }
+        catch(Exception e){
+            System.out.println(e);
+            e.printStackTrace();
+        }
+    }
+    
+}
